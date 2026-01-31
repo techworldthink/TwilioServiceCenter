f6dfb868b543377a2b8ff97f327f0d4dc7f54a3b
techworldthink <jobin25051999@gmail.com>
2026-01-31 14:30:40 +0530

    message status callback - Fix

--- relay/apps.py
+++ relay/apps.py
@@ -6,9 +6,14 @@ class RelayConfig(AppConfig):
 
     def ready(self):
         import os
+        import sys
         from . import scheduler
         
-        # Only start scheduler if we are running the server
-        # This is a common hack to prevent scheduler from running on migrate, etc.
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+        # Detect if running under Gunicorn (Production)
+        is_gunicorn = "gunicorn" in sys.argv[0]
+        
+        # Detect if running under Runserver (Dev) - check RUN_MAIN to avoid double start
+        is_runserver = "runserver" in sys.argv and (os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'))
+        
+        if is_gunicorn or is_runserver:
              scheduler.start()
